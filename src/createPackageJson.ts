f2f42e21e2470938e9cf0e2e76bc0e16ab062c67
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-09-15 02:16:53 +0000

    add babel config, change jest config

--- src/createPackageJson.ts
+++ src/createPackageJson.ts
@@ -8,17 +8,18 @@ return {
     "start": "parcel ./src/index.html",
     "dev": "parcel ./src/index.html",
     "test": "jest --verbose",
-    "coverage": "jest --coverage && codecov",
+    "coverage": "codecov",
     "build": "parcel build ./src/index.html",
     "deploy": "echo 'No deploy script has been specified'"
   },
+  "babel": {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+  }
   "jest": {
     "coverageDirectory": "./coverage/",
     "collectCoverage": true,
-    "transform": {
-      ".(ts|tsx)": "ts-jest"
-    },
-    "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+    "transform": {},
+    "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(js|jsx)$",
     "moduleNameMapper": {
       "\\.(css|scss|less|jpg|pdf|svg)$": "identity-obj-proxy"
     },
@@ -26,10 +27,10 @@ return {
       "ts",
       "tsx",
       "js",
+      "jsx"
       "jpg",
       "pdf",
       "svg"
-
     ]
   },
   "repository": {
@@ -42,26 +43,6 @@ return {
     "url": ""
   },
   "homepage": "",
-  // "dependencies": {
-  //   "@types/react": "^16.8.4",
-  //   "axios": "^0.19.0",
-  //   "node-sass": "^4.9.2",
-  //   "react": "^16.8.3",
-  //   "react-dom": "^16.8.3",
-  //   "react-inlinesvg": "^0.8.4"
-  // },
-  // "devDependencies": {
-  //   "@testing-library/react": "^8.0.1",
-  //   "@types/jest": "^24.0.13",
-  //   "codecov": "^3.5.0",
-  //   "identity-obj-proxy": "^3.0.0",
-  //   "jest": "^24.8.0",
-  //   "jest-dom": "^3.4.0",
-  //   "parcel-bundler": "^1.12.3",
-  //   "sass": "^1.17.2",
-  //   "ts-jest": "^24.0.2",
-  //   "typescript": "^3.3.3333"
-  // }
 }
 }
 
